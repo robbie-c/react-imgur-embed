17e6f767ad659bbf4d445d1f20bd08e79a0dc369
Robbie Coomber <robbie.coomber@gmail.com>
2015-12-17 13:16:32 +0000

    Add tests

--- test/tests.js
+++ test/tests.js
@@ -1,1 +1,27 @@
-require('../dist/js/ImgurEmbed.js');
+
+
+
+describe('ImgurEmbed', function() {
+  it('should render an imgur embed from an id', function () {
+
+    var testStagingId = '#test-staging1';
+
+    document.querySelector(testStagingId).innerHTML = '';
+
+    var id = 'aBHoZ3R';
+    var element = React.createElement(ImgurEmbed, {id: id});
+    React.render(element, document.querySelector(testStagingId));
+  });
+
+  it('should render multiple imgur embeds from ids', function () {
+
+    var testStagingId = '#test-staging2';
+
+    document.querySelector(testStagingId).innerHTML = '';
+
+    var id = 'aBHoZ3R';
+    var element1 = React.createElement(ImgurEmbed, {id: id, key: 1});
+    var element2 = React.createElement(ImgurEmbed, {id: id, key: 2});
+    React.render(React.DOM.div({}, [element1, element2]), document.querySelector(testStagingId));
+  });
+});
